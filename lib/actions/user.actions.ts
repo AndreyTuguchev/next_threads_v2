53d1b4a1337ec668c6524cbe0747ba65af397af1
Andrey <tuguchev.andrey@gmail.com>
2023-10-14 04:01:51 +0600

    NEW create Threads functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -37,3 +37,17 @@ export async function updateUser( { userId, username, name, bio, image, path} : 
         throw new Error(`Failed to create new user: ${error.message}`);
     }
 }
+
+export async function fetchUser ( userId: string) {
+    try {
+        connectToDB();
+
+        return await User.findOne({id: userId})
+        // .populate({
+        //     path: 'comunities',
+        //     model: Community
+        // })
+    }catch(error:any) {
+        throw new Error(`Failed to fetch user: ${error.message}`)
+    }
+}
